3e878c887f788e49ac1285480db1a90ccb129f27
djdjda <m19139347739@163.com>
2026-05-21 22:56:58 +0800

    sec_updata

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -41,12 +41,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/a1.jpg",
+			"assets/images/DesktopWallpaper/a2.jpg",
+			"assets/images/DesktopWallpaper/a3.jpg",
+			"assets/images/DesktopWallpaper/a4.jpg",
+			"assets/images/DesktopWallpaper/a5.jpg",
+			"assets/images/DesktopWallpaper/a6.jpg",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
@@ -70,7 +70,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Lovely Ace Taffy!",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
